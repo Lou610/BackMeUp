18b10fc54f98834c8bb7b5bf7ff706962030582a
TheMeeper <lou@dvbytsolutions.com>
2023-08-13 10:05:53 +0200

    forgot to add program file
    forgot to add program file

--- BackUpMe/BackUpMe/Program.cs
+++ BackUpMe/BackUpMe/Program.cs
@@ -1,11 +1,32 @@
+using BackUpMe.Services;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
 namespace BackUpMe
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<WindowsBackgroundService>();
+            IHostBuilder builder = Host.CreateDefaultBuilder(args)
+             .UseWindowsService(options =>
+             {
+                 options.ServiceName = "Back Me Up";
+             })
+             .ConfigureServices((context, services) =>
+             {
+                 LoggerProviderOptions.RegisterProviderOptions< EventLogSettings, EventLogLoggerProvider>(services);
+
+                 services.AddSingleton<JokeService>();
+                 services.AddHostedService<WindowsBackgroundService>();
+
+                 // See: https://github.com/dotnet/runtime/issues/47303
+                 services.AddLogging(builder =>
+                 {
+                     builder.AddConfiguration(
+                         context.Configuration.GetSection("Logging"));
+                 });
+             });
 
             var host = builder.Build();
             host.Run();
